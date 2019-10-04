2be95febdfefc9da94da97dbe8f93f2d5498a3af
carlossan1978 <50367557+carlossan1978@users.noreply.github.com>
2019-10-03 21:50:08 -0500

    Desarrollo capa Dominio.

---
+++ Lafrancol.SoluEventos.Controlador/Lafrancol.SoluEventos.Controlador/Controllers/Proveedor/ProveedorController.cs
@@ -0,0 +1,55 @@
+﻿using Lafrancol.SoluEventos.Dominio;
+using Lafrancol.SoluEventos.Soporte.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lafrancol.SoluEventos.Controlador.Controllers
+{
+    [RoutePrefix("api/proveedor")]
+    public class ProveedorController : ApiController
+    {
+        [HttpGet]
+        public ProveedorDto[] GetId(int id)
+        {
+            return new ProveedorDto[]
+           {
+                new ProveedorDto
+                {
+                    id = 1,
+                    nombre = "customer-fake"
+                }
+           };
+            
+        }
+        //public IHttpActionResult GetId(int id)
+        //{
+        //    var customerFake = "customer-fake";
+        //    return Ok(customerFake);
+        //}
+
+        [HttpGet]
+        public ProveedorDto[] GetAll()
+        {
+            ProveedorDto[] listaProveedores = new ProveedorDominio().buscarTodos();
+
+            //return new ProveedorDto[]
+            //{
+            //    new ProveedorDto
+            //    {
+            //        id = 1,
+            //        nombre = "Glenn Block"
+            //    },
+            //    new ProveedorDto
+            //    {
+            //        id = 2,
+            //        nombre = "Dan Roth"
+            //    }
+            //};
+            return listaProveedores;
+        }
+    }
+}
---
+++ Lafrancol.SoluEventos.Dominio/Proveedor/ProveedorDominio.cs
@@ -0,0 +1,43 @@
+﻿using Lafrancol.SoluEventos.Soporte.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lafrancol.SoluEventos.Dominio
+{
+    public class ProveedorDominio
+    {
+        public ProveedorDto[] buscarTodos()
+        {
+            //ProveedorRepository proveedorRepository = new ProveedorRepository();
+            //List<Proveedor> listaProveedores = proveedorRepository.buscarTodos();
+            //List<Proveedor> listaProveedores = 
+
+            //return listaProveedores;
+
+            return new ProveedorDto[]
+            {
+                new ProveedorDto
+                {
+                    id = 1,
+                    nombre = "Glenn Block5"
+                },
+                new ProveedorDto
+                {
+                    id = 2,
+                    nombre = "Dan Roth5"
+                }
+            };
+        }
+
+        //public Proveedor guardar(ProveedorDto proveedorAGuardarDto)
+        //{
+        //    //ProveedorRepository proveedorRepository = new ProveedorRepository();
+        //    Proveedor proveedorAGuardar = Proveedor.convertirDtoEnProveedor(proveedorAGuardarDto);
+        //    Proveedor proveedorGuardado = proveedorRepository.guardar(proveedorAGuardar);
+        //    return proveedorGuardado;
+        //}
+    }
+}
